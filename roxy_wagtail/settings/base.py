49fc6a0053c0a9711ec09a246785d35d751f04f6
Jessamyn Smith <geekchick77@gmail.com>
2015-10-18 21:12:08 -0600

    Switch to psycopg2 and dj-database-url

--- roxy_wagtail/settings/base.py
+++ roxy_wagtail/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import dj_database_url
 import os
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -91,11 +92,9 @@ WSGI_APPLICATION = 'roxy_wagtail.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# Parse database configuration from $DATABASE_URL
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 
