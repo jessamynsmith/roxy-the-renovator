bec69fc154076c14e6fa400ec014368fc33dccdf
jessamynsmith <jessamyn.smith@gmail.com>
2018-06-19 19:05:01 -0600

    Filter by regex (galleries starting with a number go in archive, ones without go on main page)

--- roxy_photologue/views.py
+++ roxy_photologue/views.py
@@ -3,14 +3,17 @@ from django.views.generic.list import ListView
 from photologue.models import Gallery
 
 
+STARTS_WITH_NUMBER_REGEX = r'^\d.*'
+
+
 class GalleryArchiveListView(ListView):
     paginate_by = 12
     template_name = 'photologue/gallery_archive_list.html'
 
     def get_queryset(self):
-        queryset = Gallery.objects.filter(is_public=False)
+        queryset = Gallery.objects.filter(title__regex=STARTS_WITH_NUMBER_REGEX)
         queryset = queryset.exclude(slug=settings.SLIDE_SHOW_SLUG)
-        queryset = queryset.on_site().order_by('title')
+        queryset = queryset.on_site().is_public().order_by('title')
         return queryset
 
 
@@ -18,5 +21,6 @@ class GalleryListView(ListView):
     paginate_by = 12
 
     def get_queryset(self):
-        queryset = Gallery.objects.on_site().is_public().order_by('date_added')
+        queryset = Gallery.objects.exclude(title__regex=STARTS_WITH_NUMBER_REGEX)
+        queryset = queryset.on_site().is_public().order_by('date_added')
         return queryset
