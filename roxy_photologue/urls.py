441d98a535f187287cf68ba82b99c49d79500420
jessamynsmith <jessamyn.smith@gmail.com>
2018-06-19 18:06:53 -0600

    Added explanatory comment on new url path

--- roxy_photologue/urls.py
+++ roxy_photologue/urls.py
@@ -8,6 +8,8 @@ from .views import GalleryArchiveListView, GalleryListView
 urlpatterns = patterns(
     '',
     url(r'', include(photologue_urls)),
+    # I should be able to have this set to /gallerylist/ and have it override the one
+    # in photologue, but it didn't work.
     url(r'^gallerylist/current/$', GalleryListView.as_view(), name='gallery-list'),
     url(r'^gallerylist/archive/$', GalleryArchiveListView.as_view(),
         name='gallery-archive-list'),
